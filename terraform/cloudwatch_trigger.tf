771486b007e10b72f9872170a5cab0f37981f942
Matthis Perrin <matthis.perrin@gmail.com>
2022-07-10 13:52:22 +0200

    Disable nzb_backfill trigger

--- terraform/cloudwatch_trigger.tf
+++ terraform/cloudwatch_trigger.tf
@@ -18,20 +18,20 @@ resource "aws_cloudwatch_event_target" "nzb_registry_trigger_target" {
 
 //
 
-resource "aws_lambda_permission" "cloudwatch_invoke_nzb_backfill" {
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.nzb_backfill.arn
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.nzb_backfill_trigger_rate.arn
-}
+# resource "aws_lambda_permission" "cloudwatch_invoke_nzb_backfill" {
+#   statement_id  = "AllowExecutionFromCloudWatch"
+#   action        = "lambda:InvokeFunction"
+#   function_name = aws_lambda_function.nzb_backfill.arn
+#   principal     = "events.amazonaws.com"
+#   source_arn    = aws_cloudwatch_event_rule.nzb_backfill_trigger_rate.arn
+# }
 
-resource "aws_cloudwatch_event_rule" "nzb_backfill_trigger_rate" {
-  name_prefix         = "nzb_backfill.cron-daily-23h30."
-  schedule_expression = "cron(30 23 ? * * *)"
-}
+# resource "aws_cloudwatch_event_rule" "nzb_backfill_trigger_rate" {
+#   name_prefix         = "nzb_backfill.cron-daily-23h30."
+#   schedule_expression = "cron(30 23 ? * * *)"
+# }
 
-resource "aws_cloudwatch_event_target" "nzb_backfill_trigger_target" {
-  rule  = aws_cloudwatch_event_rule.nzb_backfill_trigger_rate.name
-  arn   = aws_lambda_function.nzb_backfill.arn
-}
+# resource "aws_cloudwatch_event_target" "nzb_backfill_trigger_target" {
+#   rule  = aws_cloudwatch_event_rule.nzb_backfill_trigger_rate.name
+#   arn   = aws_lambda_function.nzb_backfill.arn
+# }
